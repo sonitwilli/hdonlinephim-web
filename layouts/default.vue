994f4f545547581efc860540f3b6366688c3c291
Son Bui <sonbuideveloper@gmail.com>
2018-06-06 16:45:49 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
+<body class="cms-index-index cms-home-page">
+  <div id="page">
+
+    <!-- Header -->
+    <header-menu></header-menu>
+    <!-- End Header -->
+
     <nuxt/>
+
+    <!-- Footer -->
+    <footer-component></footer-component>
+    <!-- End Footer -->
+
   </div>
+
+  <!-- mobile menu -->
+  <menu-mobile></menu-mobile>
+  <!-- end mobile menu -->
+
+</body>
 </template>
+
+<script>
+import HeaderMenu from '~/components/HeaderMenu.vue'
+import FooterComponent from '~/components/FooterComponent.vue'
+import MenuMobile from '~/components/MenuMobile.vue'
+
+export default {
+  components: {
+    HeaderMenu,
+    FooterComponent,
+    MenuMobile
+  }
+}
+</script>
+
